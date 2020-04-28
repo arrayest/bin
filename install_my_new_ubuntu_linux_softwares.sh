a083db7d74917aa8e45c36874dc74a788b815d6d
Albert <arrayest@qq.com>
2020-04-28 20:27:43 +0800

    update my shell.

--- install_my_new_ubuntu_linux_softwares.sh
+++ install_my_new_ubuntu_linux_softwares.sh
@@ -1,4 +1,4 @@
-sudo apt install -y curl vim emacs openssh-server git zsh cmus
+sudo apt install -y curl vim emacs openssh-server git zsh cmus tig htop tmux
 sudo apt install -y docker docker-compose
 sudo usermod -aG docker $USER
 newgrp - docker
