a571c7d092dfefb94f87d1fc0541cd8af9940317
Albert <arrayest@qq.com>
2020-04-28 10:43:36 +0800

    add my shell script.

---
+++ git_config.sh
@@ -0,0 +1,4 @@
+git config --global user.email "arrayest@qq.com"
+git config --global user.name "Albert"
+git config --global core.editor "vim"
+
---
+++ install_my_new_ubuntu_linux_softwares.sh
@@ -0,0 +1,1 @@
+sudo apt install -y vim openssh-server git zsh
