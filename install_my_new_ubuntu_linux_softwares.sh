d336deefa83d60c5269efdd8ccc76dad7b75c999
Albert <arrayest@qq.com>
2020-06-03 16:38:16 +0800

    update install apps shell.

--- install_my_new_ubuntu_linux_softwares.sh
+++ install_my_new_ubuntu_linux_softwares.sh
@@ -1,4 +1,4 @@
-sudo apt install -y curl vim emacs openssh-server git zsh cmus tig htop tmux ffmpeg vlc tree flex p7zip-full squashfs-tools-ng
+sudo apt install -y curl vim emacs openssh-server git zsh cmus tig htop tmux ffmpeg vlc tree flex p7zip-full squashfs-tools-ng cmake libcairo2-dev
 sudo apt install -y docker docker-compose
 sudo usermod -aG docker $USER
 newgrp - docker
